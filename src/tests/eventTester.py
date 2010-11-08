ec49e762a30466a2bf081d7b52d5f3cfb803c92c
Pekka Salmela <pekka.salmela@gmail.com>
2010-11-08 21:15:21 +0200

    Fixed tests

--- src/tests/eventTester.py
+++ src/tests/eventTester.py
@@ -1,3 +1,11 @@
 class EventTester:
+    def __init__(self):
+        self.lastEvent = None
+    
     def Notify(self, ev):
         self.lastEvent = ev
+        
+    def checkLastEvent(self):
+        result = self.lastEvent
+        self.lastEvent = None
+        return result 
--- src/tests/test_charactor.py
+++ src/tests/test_charactor.py
@@ -9,6 +9,7 @@ from eventTester import EventTester
 from shallowSpace.actors import Charactor
 from shallowSpace.eventManager import EventManager
 from shallowSpace.game import Game
+from shallowSpace.event import CharactorMoveEvent
 
 class BasicTests(unittest.TestCase):
 
@@ -17,6 +18,7 @@ class BasicTests(unittest.TestCase):
         self.eventManager = em
         self.game = Game(em)
         self.eventTester = EventTester()
+        self.eventManager.RegisterListener(self.eventTester)
 
 
     def tearDown(self):
@@ -29,10 +31,13 @@ class BasicTests(unittest.TestCase):
         map.Build()
         c.Place(map.sectors[map.startSectorIndex])
         c.Move(shallowSpace.constants.DIRECTION_RIGHT)
-        
+        self.assertTrue(isinstance(self.eventTester.checkLastEvent(), CharactorMoveEvent))
         c.Move(shallowSpace.constants.DIRECTION_DOWN) 
+        self.assertTrue(isinstance(self.eventTester.checkLastEvent(), CharactorMoveEvent))
         c.Move(shallowSpace.constants.DIRECTION_LEFT) 
+        self.assertTrue(isinstance(self.eventTester.checkLastEvent(), CharactorMoveEvent))
         c.Move(shallowSpace.constants.DIRECTION_UP) 
+        self.assertTrue(isinstance(self.eventTester.checkLastEvent(), CharactorMoveEvent))
         sec = map.sectors[map.startSectorIndex]
         newSec = c.sector
         self.assertEqual(map.sectors.index(sec), map.sectors.index(newSec))
