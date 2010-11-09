9c47d7ef548a5e96c8e8158769f9e51a3bf395c7
Pekka Salmela <pekka.salmela@gmail.com>
2010-11-09 17:55:46 +0200

    Removed absolute paths

--- src/shallowSpace/eventManager.py
+++ src/shallowSpace/eventManager.py
@@ -24,7 +24,6 @@ class EventManager:
     def UpdateListeners(self):
         for listener in self.newListeners:
             self.RegisterListener(listener)
-            debug(listener)
         self.newListeners = []
             
     #----------------------------------------------------------------------
--- src/shallowSpace/shallowSpace.py
+++ src/shallowSpace/shallowSpace.py
@@ -7,14 +7,25 @@ Created on Oct 31, 2010
 from game import Game
 from pygameview import PygameView
 from eventManager import EventManager
+import ConfigParser
+import os
+import constants
 
 #------------------------------------------------------------------------------
 from controllers import KeyboardController, CPUSpinnerController
+import shallowSpace
 
 def main():
     """Main program to start the game"""
     
     evManager = EventManager()
+
+    programPath = os.path.join(os.path.dirname(shallowSpace.__file__))
+    confFilePath = os.path.abspath(programPath, "../../config/config.cfg")
+    config = ConfigParser.ConfigParser()
+    config.read(confFilePath)
+    config.set("Images", "rootdir", os.path.abspath(programPath, "../../"))
+    constants.CONFIG = config
 
     keybd = KeyboardController( evManager )
     spinner = CPUSpinnerController( evManager )
