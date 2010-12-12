c3b53e0ea301ad020f976ab474112e09bd793d8d
Pekka Salmela <pekka.salmela@gmail.com>
2010-12-12 20:57:23 +0200

    Listener groups for event manager

--- src/shallowspace/eventmanager.py
+++ src/shallowspace/eventmanager.py
@@ -1,30 +1,46 @@
-from utils import *
-from event import TickEvent, BulletsMoveEvent
+from utils import debug
+from event import TickEvent
 
 class EventManager:
     """this object is responsible for coordinating most communication
     between the Model, View, and Controller."""
 
-    def __init__(self ):
-        from weakref import WeakKeyDictionary
-        self.listeners = WeakKeyDictionary()
+    def __init__(self):
+        self.listenerGroups = {"default":ListenerGroup()}
 
-    #----------------------------------------------------------------------
-    def register_listener( self, listener ):
-        self.listeners[ listener ] = 1
+    def register_listener(self, listener, groups=[]):
+        if len(groups) == 0:
+            groups = ["default"]
+        for groupName in groups:
+            if groupName in self.listenerGroups:     
+                self.listenerGroups[groupName].add_listener(listener)
+            else:
+                self.listenerGroups[groupName] = ListenerGroup()
+                self.listenerGroups[groupName].add_listener(listener)
 
-    #----------------------------------------------------------------------
-    def unregister_listener( self, listener ):
-        if listener in self.listeners:
-            del self.listeners[ listener ]
+    def unregister_listener(self, listener):
+        for group in self.listenerGroups:
+            if listener in group.listeners:
+                del self.listeners[listener]
             
-    #----------------------------------------------------------------------
-    def post( self, event ):
-        if not isinstance(event, TickEvent) and not isinstance(event, BulletsMoveEvent):
+    def post(self, event, groups=[]):
+        if not isinstance(event, TickEvent):
             debug( "     Message: " + event.name )
             
-        for listener in self.listeners:
-            #NOTE: If the weakref has died, it will be 
-            #automatically removed, so we don't have 
-            #to worry about it.
-            listener.notify( event )
+        for groupName in groups:
+            listenerGroup = self.listenerGroups[groupName].listeners
+            for listener in listenerGroup:
+                #NOTE: If the weakref has died, it will be automatically removed, so we don't have 
+                #to worry about it.
+                listener.notify(event)
+        for listener in self.listenerGroups["default"].listeners:
+            listener.notify(event)
+
+class ListenerGroup:
+    def __init__(self):
+        from weakref import WeakKeyDictionary
+        self.listeners = WeakKeyDictionary()
+        
+    def add_listener(self, listener):
+        self.listeners[listener] = 1
+        
